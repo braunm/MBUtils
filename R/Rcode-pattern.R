27136003c546dca1885c604583ff8f914e07c184
Michael Braun <braunm@smu.edu>
2019-12-03 10:15:56 -0600

    added patterns for Rmd files

--- R/Rcode-pattern.R
+++ R/Rcode-pattern.R
@@ -12,6 +12,17 @@ Rcode_pattern <- list(
   ref.chunk = "^\\s*\\\\RcodeChunk\\{(.+)\\}.*$"
 )
 
+#' @name rmd_pattern
+#' @title Definition of the Rmd pattern
+#' @details A named list defining the rmd pattern
+#' @export
+Rmd_pattern <- list(
+  chunk.begin = "^[\t >]*```+\\s*\\{([a-zA-Z0-9_]+( *[ ,].*)?)\\}\\s*$",
+  chunk.end = "^[\t >]*```+\\s*$",
+  ref.chunk = "^\\s*<<(.+)>>\\s*$",
+  inline.code = "(?<!(^|\n)``)`r[ #]([^`]+)\\s*`"
+)
+
 
 #' @rdname rcode_pattern
 #' @title Functions to set the rcode pattern before knitting or rendering.
@@ -22,12 +33,17 @@ Rcode_pattern <- list(
 #' This will work with Rnw files.  So, to compile an Rnw file with
 #'   Rcode blocks, run pat_rcode() first.  To switch back to Sweave
 #'   syntax (or really, any other syntax), run
-#'   knit_patterns#restore(), and let knitr figure it out. Or, run
+#'   knit_patterns$restore(), and let knitr figure it out. Or, run
 #'   pat_zzz(), where zzz is an extension for the various convenience
 #'   functions provided by knitr.
 #' @export
 pat_rcode <- function() {
   knit_patterns$restore(Rcode_pattern)
+}
+#' @rdname rmd_pattern
+#' @export
+pat_rmd <- function() {
+  knit_patterns$restore(Rmd_pattern)
 }
 
 #' @export pat_rlatex pat_Rcode
@@ -35,5 +51,9 @@ pat_rcode <- function() {
 pat_rlatex <- pat_rcode
 #' @rdname rcode_pattern
 pat_Rcode <- pat_rcode
+
+#' @export pat_Rmd
+#' @rdname rmd_pattern
+pat_Rmd <- pat_rmd
 
 
