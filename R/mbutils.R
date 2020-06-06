ab9bc119de897054cf28e733930fc0a870dfed35
Michael Braun <braunm@smu.edu>
2020-06-06 13:48:44 -0500

    added numerical routines that were previously in CppADutils

---
+++ R/mbutils.R
@@ -0,0 +1,12 @@
+#' @name mbutils-package
+#' @aliases mbutils-package
+#' @docType package
+#' @title Various utility functions.
+#' @description Later.
+#' @details More later.
+#' @keywords package
+#'
+#' @useDynLib mbutils, .registration=TRUE
+#' @importFrom Rcpp evalCpp
+#' @import methods
+NULL
